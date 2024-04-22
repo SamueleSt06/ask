0f9bedce620c000634438b10565628bc4e5d6a0f
im_samust <114179474+SamueleSt06@users.noreply.github.com>
2024-04-22 10:33:42 +0200

    Add files via upload

---
+++ password.js
@@ -0,0 +1,8 @@
+function showPwd() {
+    var input = document.getElementById('pwd');
+    if (input.type === "password") {
+      input.type = "text";
+    } else {
+      input.type = "password";
+    }
+  }
